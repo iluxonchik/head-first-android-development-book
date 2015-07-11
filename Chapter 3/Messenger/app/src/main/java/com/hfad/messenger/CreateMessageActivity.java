0c406d6effe325f54dffa2564869629f6d97d14c
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-07-11 16:28:26 +0100

    Chapter 3 completed (Part 2 - Actions)

--- Chapter 3/Messenger/app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
+++ Chapter 3/Messenger/app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -18,11 +18,25 @@ public class CreateMessageActivity extends Activity {
     // Call onSendMessage() when the button is clicked
     public void onSendMessage(View view) {
         EditText editText = (EditText)findViewById(R.id.message);
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-
         String messageText = editText.getText().toString();
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
 
-        startActivity(intent); // tell Android to start the activity specified in the intent
+        EditText subject = (EditText)findViewById(R.id.subject);
+        String subjectText = subject.getText().toString();
+
+
+        Intent intent = new Intent(Intent.ACTION_SEND); // create and implicit intent with "send" action
+        intent.setType("text/plain"); // set MIME type to text plain
+        intent.putExtra(Intent.EXTRA_TEXT, messageText); // put the extra to send
+        intent.putExtra(Intent.EXTRA_SUBJECT, subjectText);
+
+        // force app selection screen
+        /*
+            Android asks the user which app he wants to run, this time with no "Always Use This App"
+            option and returns an explicit intent associated with the user-chosen activity.
+
+            The explicit intent is stored in "explicitChooserIntent" var
+         */
+        Intent explicitChooserIntent = Intent.createChooser(intent, getString(R.string.chooser));
+        startActivity(explicitChooserIntent); // tell Android to start the activity specified in the intent
     }
 }
