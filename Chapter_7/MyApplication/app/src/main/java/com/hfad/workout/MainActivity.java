86ccdf6694137ce5b3a641146f4808c65dddeb65
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-07-17 14:56:22 +0100

    Chapter 7 app completed.

--- Chapter_7/MyApplication/app/src/main/java/com/hfad/workout/MainActivity.java
+++ Chapter_7/MyApplication/app/src/main/java/com/hfad/workout/MainActivity.java
@@ -2,10 +2,12 @@ package com.hfad.workout;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListListener{
@@ -18,14 +20,27 @@ public class MainActivity extends Activity implements WorkoutListFragment.Workou
 
     @Override
     public void itemClicked(long id) {
-        WorkoutDetailFragment details = new WorkoutDetailFragment();
-        FragmentTransaction ft = getFragmentManager().beginTransaction(); // begin transaction
-        details.setWorkoutId(id);
-        ft.replace(R.id.fragment_container, details); // replace the fragment in the R.id.fragement_container
-                                                     // view with the newly created "details" fragment
-        ft.addToBackStack(null); // add the fragment to the back stack so that we cam returm to it
-                                // using the back button
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit(); // commit the transaction (apply changes, all at once)
+        /*
+            LOGIC: if the view has a <FragmentContainer> it means that the xml in layout-large
+            folder is being used, otherwise the one in layout folder is.
+         */
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if(fragmentContainer != null) {
+            // Tablet view in use, simply replace the fragment in <FragmentContainer>
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction(); // begin transaction
+            details.setWorkoutId(id);
+            ft.replace(R.id.fragment_container, details); // replace the fragment in the R.id.fragment_container
+            // view with the newly created "details" fragment
+            ft.addToBackStack(null); // add the fragment to the back stack so that we cam return to it
+            // using the back button
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit(); // commit the transaction (apply changes, all at once)
+        } else {
+            // Phone view in use, start a new activity
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
     }
 }
--- Chapter_7/MyApplication/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ Chapter_7/MyApplication/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -20,6 +20,9 @@ public class WorkoutDetailFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if(savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
@@ -34,6 +37,11 @@ public class WorkoutDetailFragment extends Fragment {
             TextView description = (TextView) view.findViewById(R.id.textDescription);
             description.setText(workout.getDescription());
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong("workoutId", workoutId);
     }
 
     public void setWorkoutId(long id) {
